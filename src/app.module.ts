4f606960704a008cb1deed55147282e0e1e953e1
yuraludchak <yura.ludchak@ralabs.org>
2025-02-14 14:34:17 +0200

    init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,25 @@
 import {MiddlewareConsumer, Module, NestModule} from '@nestjs/common';
 import {ConfigModule} from "@nestjs/config";
+import { AuthModule } from './auth/auth.module';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
+import { MessagesModule } from './messages/messages.module';
 
 @Module({
     imports: [
         ConfigModule.forRoot({
           isGlobal: true
         }),
+        AuthModule,
+        UsersModule,
+        MessagesModule,
     ],
+    providers: [UsersService],
 })
 export class AppModule implements NestModule {
     configure(consumer: MiddlewareConsumer) {
         consumer
             .apply()
-            .forRoutes('whiskey');
+            .forRoutes('');
     }
 }
---
+++ src/auth/constants.ts
@@ -0,0 +1,3 @@
+export const jwtConstants = {
+    secret: 'ILyPmYJsg00DCoCi2YXBFPRhLL9PZmOzi0ac2gACldAUNGNStScnhVESUU46j5l1',
+};
