d7d92bd4ba35d7414d949ac863a964c4c95c2c26
yuraludchak <yura.ludchak@ralabs.org>
2025-02-17 11:08:00 +0200

    clear project

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,5 +14,5 @@ export class AppModule implements NestModule {
                                                 consumer
                                                     .apply()
                                                     .forRoutes('');
-                                            }
+                            }
 }
