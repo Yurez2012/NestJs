c04ca0773c1073c7cf8322cb29bdfdc86d531d79
yuraludchak <yura.ludchak@ralabs.org>
2025-02-17 11:33:15 +0200

    clear project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,23 @@
-import {MiddlewareConsumer, Module, NestModule} from '@nestjs/common';
-import {ConfigModule} from "@nestjs/config";
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [
-        ConfigModule.forRoot({
-            isGlobal: true
-        }),
-    ],
-    providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
+  providers: [],
 })
 export class AppModule implements NestModule {
-    configure(consumer: MiddlewareConsumer) {
+  configure(consumer: MiddlewareConsumer) {
 
 
 
+    consumer.apply().forRoutes('');
 
 
-                                                consumer
-                                                    .apply()
-                                                    .forRoutes('');
-                            }
+
+    
+  }
 }
