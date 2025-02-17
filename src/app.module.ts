a429980018eebdfd44a772edd97cc51f6bc9a7c1
yuraludchak <yura.ludchak@ralabs.org>
2025-02-17 10:33:01 +0200

    clear project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,13 @@
 import {MiddlewareConsumer, Module, NestModule} from '@nestjs/common';
 import {ConfigModule} from "@nestjs/config";
-import { AuthModule } from './auth/auth.module';
-import { UsersService } from './users/users.service';
-import { UsersModule } from './users/users.module';
-import { MessagesModule } from './messages/messages.module';
 
 @Module({
     imports: [
         ConfigModule.forRoot({
           isGlobal: true
         }),
-        AuthModule,
-        UsersModule,
-        MessagesModule,
     ],
-    providers: [UsersService],
+    providers: [],
 })
 export class AppModule implements NestModule {
     configure(consumer: MiddlewareConsumer) {
--- src/auth/constants.ts
+++
@@ -1,3 +0,0 @@
-export const jwtConstants = {
-    secret: 'ILyPmYJsg00DCoCi2YXBFPRhLL9PZmOzi0ac2gACldAUNGNStScnhVESUU46j5l1',
-};
